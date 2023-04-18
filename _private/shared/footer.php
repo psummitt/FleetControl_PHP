e983db8f58ec93124621ed111f48b4d199cd246e
Paul M. Summitt <psummitt@gmail.com>
2023-04-18 13:57:23 -0500

    update

---
+++ _private/shared/footer.php
@@ -0,0 +1,21 @@
+<footer class="bg-dark link-light">
+            <div>
+            <!-- Social media and contact links. Add or remove any networks. -->
+                <ul>
+                    <li><a href="mailto:paulmsummitt@gmail.com"><img src="https://res.cloudinary.com/dhfzm8v7y/image/upload/v1656777427/images/opened-email-envelope_icon-icons.com_70656_gx2u5j.png" style="color: white" title="e-mail" width="20px" height="20px">paulmsummitt@gmail.com</a></li>
+                    <li><a href="http://paulmsummitt.wordpress.com" target="_blank" rel="noopener"><img src="https://res.cloudinary.com/dhfzm8v7y/image/upload/v1656800093/images/104474_wordpress_blog_icon_yxsxrt.png" style="color: white" title="WordPress"  width="20px" height="20px">paulmsummitt.wordpress.com</a></li>
+                    <li><a href="http://twitter.com/Paul_M_Summitt" target="_blank" rel="noopener">
+                    <img src="https://res.cloudinary.com/dhfzm8v7y/image/upload/v1656777427/images/Twitter_Rounded_icon-icons.com_61577_tpqlp4.png" style="color: white" title="Twitter"  width="20px" height="20px">Twitter</a></li>
+                    <li><a href="https://www.linkedin.com/in/paul-m-summitt/" target="_blank" rel="noopener">
+                    <img src="https://res.cloudinary.com/dhfzm8v7y/image/upload/v1656777431/images/LinkedIn_Rounded_icon-icons.com_61573_fhhjjs.png" style="color: white" title="LinkedIn"  width="20px" height="20px">LinkedIn</a></li>
+                </ul>
+            </div>
+                <p id="textbottom">&copy; <?php echo date('Y'); ?> Paul M. Summitt</p>
+            </div>
+        </footer>
+    </body>
+</html>
+<?php
+    db_disconnect($db);
+?>
+
---
+++ index.php
@@ -0,0 +1,27 @@
+<?php require_once('./_private/initialize.php'); ?>
+
+<?php $page_title = 'Staff Menu'; ?>
+<?php include(SHARED_PATH . '/header.php'); ?>
+
+<article>
+    <div class="container text-center">
+        <div class="row">
+            <h1>FleetControl</h1>
+            <h2>Your Online Vehicle Management System</h2>
+        </div>
+
+    </div>
+    <div class="row">
+        <div class="col-4" id="img">
+            <img class="img-fluid" src="./img/Truck_368x368.jpg" alt="image of a truck">
+        </div>
+        <div class="col-8" id="content">
+            <hr>
+            <h3>Welcome to FleetControl.</h3>
+            <hr>
+            <p>You can click the menu items above to view or add information about your vehicles.</p>
+        </div>
+    </div>
+</article>
+
+<?php include(SHARED_PATH . '/footer.php'); ?>
