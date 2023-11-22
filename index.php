13cd344de2e0ce92c58325db35dae86f5682d172
Paul M. Summitt <psummitt@gmail.com>
2023-11-22 08:20:17 -0600

    updated css

--- index.php
+++ index.php
@@ -5,47 +5,46 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FleetControl</title>
-    <link rel="stylesheet" href="/public/css/styles.css">
-    <script src="/public/js/index.js"></script>
+    <link rel="stylesheet" href="public/css/styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="public/js/index.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </head>
 <body>
 <header>
-
-</header>
-
-<article>
     <div class="container text-center">
         <div class="row">
-            <h1>FleetControl</h1>
-            <h2>Your Online Vehicle Management System</h2>
+            <h1>Welcome to FleetControl</h1>
         </div>
-
     </div>
+</header>
+<article>
     <div class="row">
         <div class="col-4" id="img">
             <img class="img-fluid" src="public/img/Truck_368x368.jpg" alt="image of a truck">
         </div>
         <div class="col-8" id="content">
             <hr>
-            <h3>Welcome to FleetControl.</h3>
+            <h2>FleetControl</h2>
+            <hr>
+            <h3>Your Online Vehicle Management System</h3>
             <hr>
             <p>Please log in to view or add information about your vehicles.</p>
+            <hr>
+            <form action="/FleetControl/_private/authenticate.php" method="post">
+                <label for="username">
+                    <i class="fas fa-user"></i>
+                </label>
+                <input type="text" name="username" placeholder="Username" id="username" required>
+                <label for="password">
+                    <i class="fas fa-lock"></i>
+                </label>
+                <input type="password" name="password" placeholder="Password" id="password" required>
+                <input type="submit" value="Login">
+            </form>
+            <hr>
         </div>
-        <hr>
-        <form action="/FleetControl/_private/authenticate.php" method="post">
-            <label for="username">
-                <i class="fas fa-user"></i>
-            </label>
-            <input type="text" name="username" placeholder="Username" id="username" required>
-            <label for="password">
-                <i class="fas fa-lock"></i>
-            </label>
-            <input type="password" name="password" placeholder="Password" id="password" required>
-            <input type="submit" value="Login">
-        </form>
-        <hr>
     </div>
 </article>
 <footer class="bg-dark link-light">
