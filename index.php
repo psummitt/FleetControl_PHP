bc02b9469d2564e0d39218845655a39d82e73274
Paul M. Summitt <psummitt@gmail.com>
2024-01-04 07:49:56 -0600

    modified footer

--- index.php
+++ index.php
@@ -51,12 +51,10 @@
     <div>
         <!-- Social media and contact links. Add or remove any networks. -->
         <ul>
-            <li><a href="mailto:paulmsummitt@gmail.com"><img src="https://res.cloudinary.com/dhfzm8v7y/image/upload/v1656777427/images/opened-email-envelope_icon-icons.com_70656_gx2u5j.png" style="color: white" title="e-mail" width="20px" height="20px">paulmsummitt@gmail.com</a></li>
-            <li><a href="http://paulmsummitt.wordpress.com" target="_blank" rel="noopener"><img src="https://res.cloudinary.com/dhfzm8v7y/image/upload/v1656800093/images/104474_wordpress_blog_icon_yxsxrt.png" style="color: white" title="WordPress"  width="20px" height="20px">paulmsummitt.wordpress.com</a></li>
+            <li><a href="mailto:paulmsummitt@gmail.com"><img src="https://res.cloudinary.com/dhfzm8v7y/image/upload/v1656777427/images/opened-email-envelope_icon-icons.com_70656_gx2u5j.png" style="color: white" title="e-mail" width="20" height="20">paulmsummitt@gmail.com</a></li>
             <li><a href="https://www.linkedin.com/in/paul-m-summitt/" target="_blank" rel="noopener">
-                    <img src="https://res.cloudinary.com/dhfzm8v7y/image/upload/v1656777431/images/LinkedIn_Rounded_icon-icons.com_61573_fhhjjs.png" style="color: white" title="LinkedIn"  width="20px" height="20px">LinkedIn</a></li>
+                    <img src="https://res.cloudinary.com/dhfzm8v7y/image/upload/v1656777431/images/LinkedIn_Rounded_icon-icons.com_61573_fhhjjs.png" style="color: white" title="LinkedIn"  width="20" height="20">LinkedIn</a></li>
         </ul>
-    </div>
     <p id="textbottom">&copy; <?php echo date('Y'); ?> Paul M. Summitt</p>
     </div>
 </footer>
