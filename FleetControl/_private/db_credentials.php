abc824a45bbfad276612dda1b7a5c80ee1d64fde
psummitt <psummitt@gmail.com>
2024-05-22 20:42:49 -0500

    update

--- FleetControl/_private/db_credentials.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-const DB_SERVER = "localhost";
-const DB_USER = "webuser";
-const DB_PASS = "secretpassword";
-const DB_NAME = "FleetControl";
-
-?>
--- FleetControl/src/FleetControl/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * @author paulmsummitt
- *
- */
-package FleetControl;
